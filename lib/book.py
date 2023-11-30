70b02dc7dbaf1b6b74c44f51baf7bde09105f4af
james <jimmindungu3@gmail.com>
2023-11-30 08:50:44 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,14 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
+    # Constructor method (__init__) initializes the instance with title and page_count
+    def __init__(self, title, page_count):
+        # Check if page_count is an integer, otherwise print an error message
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+        else:
+            self.title = title
+            self.page_count = page_count
+
+    # Method to simulate turning a page
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
-
+# Create a Shoe class in shoe.py
 class Shoe:
-    pass
+    # Constructor method (__init__) initializes the instance with brand and size
+    def __init__(self, brand, size):
+        # Check if size is an integer, otherwise print an error message
+        if not isinstance(size, int):
+            print("size must be an integer")
+        else:
+            self.brand = brand
+            self.size = size
+
+    # Method to indicate that the shoe has been repaired
+    def repair_shoe(self):
+        print("The shoe has been repaired.")
+        # Create an attribute 'condition' and set it to 'New' after repair
+        self.condition = 'New'
+        
